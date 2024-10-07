ad56abe5964b3f0a9ebce71266183b7796fedd79
Allan <allan.bernier1@gmail.com>
2024-10-07 10:51:53 +0200

    exemple-app

---
+++ exemple-app/routes/api.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+Route::get("/hello-world", function () {
+    return response()->json([
+        'message' => 'Hello from api'
+    ]);
+});
+
+
+
+// localhost:8000/api/hello/toto
+
+Route::get("/hello/{name}", function (String $name) {
+    return response()->json([
+        'message' => 'Hello from ' . $name
+    ]);
+});
+
+
+Route::get("/hello-request-param", function (Request $request) {
+    $name = $request->name;
+    
+    return response()->json([
+        'message' => 'Hello from ' . $name
+    ]);
+});
